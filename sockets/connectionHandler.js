0e4da02f88e3d29a6c84718d92449f45e86064fb
Moosa <mawad@student.42abudhabi.ae>
2025-04-24 16:32:30 +0400

    Modified socket connection logic to allow a single user to connect from multiple sockets at once (useful when a user connects to our website from multiple windows as that creates separate connections. Implemented a notification system that alerts the user whether they have recieved a friend request and if their request gets accepted. Next step is create a notifications table which stores notifications that couldn't be sent right away, a scenario that can occur when the recipient is offline.

---
+++ sockets/connectionHandler.js
@@ -0,0 +1,83 @@
+const { verifySignedCookie } = require("../utils/cookies");
+const jwt = require("jsonwebtoken");
+
+const onlineUsers = new Map();
+
+function handleSocketConnection(io) {
+	io.on("connection", (socket) => {
+		const rawCookieHeader = socket.handshake.headers.cookie;
+		if (!rawCookieHeader) {
+			console.log(
+				"No accessToken cookie was fond OR client did not initialize socket with the following option withCredentials: true"
+			);
+			socket.disconnect();
+			return;
+		}
+
+		const token = verifySignedCookie(
+			rawCookieHeader,
+			"accessToken",
+			process.env.COOKIE_SECRET
+		);
+		if (!token) {
+			console.log("No accessToken cookie found");
+			socket.disconnect();
+			return;
+		}
+
+		try {
+			const payload = jwt.verify(token, process.env.JWT_SECRET);
+			const userId = payload.user.id;
+
+			if (!onlineUsers.has(userId)) {
+				onlineUsers.set(userId, new Set());	// Note 1
+			}
+			onlineUsers.get(userId).add(socket.id);
+			console.log(`User ${userId} connected with socket ${socket.id}`);
+
+			// Handle disconnection (This code block is placed here because we only want register this event on the client's socket only if they logged in successfuly )
+			socket.on("disconnect", () => {
+				const userSockets = onlineUsers.get(userId);
+				if (userSockets) {
+					userSockets.delete(socket.id); // delete the socket's id from the Map when a socket disconnects to clean up memory.
+					if (userSockets.size === 0) {
+						// If userSockets is now empty as a result of the delete, clean up the empty set from the onlineUsers map
+						onlineUsers.delete(userId);
+					}
+				}
+				console.log(
+					`User ${userId} disconnected from socket ${socket.id}`
+				);
+			});
+		} catch (err) {
+			console.log("Invalid token:", err.message);
+			socket.disconnect();
+		}
+	});
+}
+
+module.exports = {
+	onlineUsers,
+	handleSocketConnection,
+};
+
+/*
+	NOTES
+
+	Note 1
+
+		Why are we using a set here? Well, if a client opens the same page from the same browser but on two separate 
+		windows, that is two separate socket connections. So if we do not take that into account by not storing all 
+		his sockets that he is connected to our server endpoint from, then he would only get notifications/messages 
+		on one window and it looks awkward. Thus, we avoid that by not just having a map that "maps" userId's to a 
+		single socket.id, but to potentially multiple socket.id's to account for the aforementioned scenario properly.
+		
+		Quick overview of a set and how it differs from a data structure like an array:
+
+			Feature 			| Array 						 	 | Set
+			Duplicates allowed 	| ✅ Yes 	 						| ❌ No
+			Fast lookup/remove 	| ❌ Needs .indexOf() or filter 		| ✅ Native .has() / .delete()
+			Useful when 		| Order matters / duplicates okay 	 | Unique values, fast ops
+
+*/
+
